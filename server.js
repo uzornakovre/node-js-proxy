7851e0351ebe2c7ad3e3e2b6abf664b948df2851
Konstantin Filyaev <filyaev.k@wb.ru>
2025-01-28 22:25:16 +0300

    feat: add socks5

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const app = express();
 
 const { PORT, HOST } = require("./config");
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
+// app.get("/ping", (req, res) => {
+//   res.send("pong");
+// });
+
+// app.listen(PORT, HOST, () => {
+//   console.log(`Сервер запущен на порту ${PORT}`);
+// });
+
+const socks5 = require("socks5");
+
+const server = socks5.createServer((info, accept, deny) => {
+  const socket = accept(true);
+  if (socket) {
+    console.log("SOCKS5-соединение установлено:", info.dstAddr, info.dstPort);
+  }
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+server.listen(PORT, HOST, () => {
+  console.log(`SOCKS5-прокси-сервер запущен на порту ${PORT}`);
 });
