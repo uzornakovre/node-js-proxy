ac3e94f97c766f8b236107240ae1c305d0e69b5d
Konstantin Filyaev <filyaev.k@wb.ru>
2025-01-31 02:24:38 +0300

    feat: disable secure

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const fs = require("fs");
-const https = require("https");
 const http = require("http");
 const httpProxy = require("http-proxy");
 
-const { PORT, HOST, PRIVKEY_PATH, CERT_PATH, CHAIN_PATH } = require("./config");
+const { PORT, HOST } = require("./config");
 
-const proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer({
+  secure: false,
+});
 
-const options = {
-  key: fs.readFileSync(PRIVKEY_PATH),
-  cert: fs.readFileSync(CERT_PATH),
-  ca: fs.readFileSync(CHAIN_PATH),
-};
-
-const server = https.createServer(options, (req, res) => {
+const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
 
-  proxy.web(req, res, { target: req.url, changeOrigin: true }, (err) => {
+  const target = `${req.headers["x-original-scheme"] || "https"}://${
+    req.headers.host
+  }${req.url}`;
+
+  proxy.web(req, res, { target, changeOrigin: true }, (err) => {
     console.error("Ошибка:", err);
     res.writeHead(502);
     res.end("Прокси-сервер: ошибка соединения");
@@ -24,16 +22,38 @@ const server = https.createServer(options, (req, res) => {
 });
 
 server.listen(PORT, HOST, () => {
-  console.log(`HTTPS-прокси запущен на порту ${PORT}`);
+  console.log(`HTTP-прокси запущен на порту ${PORT}`);
 });
 
-// const express = require("express");
-// const app = express();
+// const fs = require("fs");
+// const https = require("https");
+// const http = require("http");
+// const httpProxy = require("http-proxy");
 
-// app.get("/ping", (req, res) => {
-//   res.send("pong");
+// const { PORT, HOST, PRIVKEY_PATH, CERT_PATH, CHAIN_PATH } = require("./config");
+
+// const proxy = httpProxy.createProxyServer({});
+
+// const options = {
+//   key: fs.readFileSync(PRIVKEY_PATH),
+//   cert: fs.readFileSync(CERT_PATH),
+//   ca: fs.readFileSync(CHAIN_PATH),
+// };
+
+// const server = https.createServer(options, (req, res) => {
+//   console.log(`${req.method} ${req.url}`);
+
+//   proxy.web(req, res, { target: req.url, changeOrigin: true }, (err) => {
+//     console.error("Ошибка:", err);
+//     res.writeHead(502);
+//     res.end("Прокси-сервер: ошибка соединения");
+//   });
 // });
 
-// app.listen(PORT, HOST, () => {
-//   console.log(`Сервер запущен на порту ${PORT}`);
+// server.listen(PORT, HOST, () => {
+//   console.log(`HTTPS-прокси запущен на порту ${PORT}`);
 // });
+
+// PRIVKEY_PATH='/etc/letsencrypt/live/uzornakovre.freemyip.com/privkey.pem'
+// CERT_PATH='/etc/letsencrypt/live/uzornakovre.freemyip.com/cert.pem'
+// CHAIN_PATH='/etc/letsencrypt/live/uzornakovre.freemyip.com/chain.pem'
